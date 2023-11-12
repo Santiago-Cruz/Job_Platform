08e40ba7c7679362d1f3d78712ff7e4c3c466d35
John Fredy Correa <johnfredycorrea75@gmail.com>
2023-11-11 19:54:57 -0600

    se agrega el carrito y estado en el schema

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,27 @@ import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
 import RegistroForm from './components/RegistroForm';
 import IngresoForm from './components/IngresoForm';
 
+
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="api/users/users" element={<RegistroForm />} />
-        <Route path="/ingreso/verificar" element={<IngresoForm />} />
-        <Route
-          exact path="/"
-          element={
-            <div>
-              <Link to="api/users/users">
-                <button>Registro</button>
-              </Link>
-              <Link to="/ingreso/verificar">
-                <button>Ingreso</button>
-              </Link>
-            </div>
-          }
-        />
-      </Routes>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <div>
+                <Link to="/api/users/users">
+                  <button>Registro</button>
+                </Link>
+                <Link to="/api/users/ingreso-verificar">
+                  <button>Ingreso</button>
+                </Link>
+              </div>
+            }
+          />
+          <Route path="/api/users/users" element={<RegistroForm />} />
+          <Route path="/api/users/ingreso-verificar" element={<IngresoForm />} />      
+        </Routes>
     </BrowserRouter>
   );
 }
